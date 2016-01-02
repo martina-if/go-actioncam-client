3a51d6ca1d32a02d2745d615d01769ba787d2431
Martina Iglesias <martina@spotify.com>
2016-01-02 19:59:40 +0100

    Add other commands

--- main.go
+++ main.go
@@ -12,11 +12,15 @@ type Command struct {
 	ParamSize int `json:"param_size"`
 }
 
+var CONNECT = Command{Token: 1, MsgId: 1, ParamSize: 1}
+var UNKNOWN = Command{Token: 1, MsgId: 2, ParamSize: 0}
+var CAPTURE = Command{Token: 1, MsgId: 5, ParamSize: 0}
+var START_RECORDING = Command{Token: 1, MsgId: 3, ParamSize: 0}
+var STOP_RECORDING = Command{Token: 1, MsgId: 4, ParamSize: 0}
+
 func main() {
 
-	connectCmd := Command{Token: 1, MsgId: 1, ParamSize: 1}
-
-	fmt.Println("Sending command: ", connectCmd)
+	fmt.Println("Sending command: ", CONNECT)
 	conn, err := net.Dial("tcp", "localhost:7878")
 	if err != nil {
 		fmt.Println("Error connecting to camera: ", err)
@@ -24,7 +28,7 @@ func main() {
 	}
 
 	reader := bufio.NewReader(conn)
-	SendCommand(conn, reader, connectCmd)
+	SendCommand(conn, reader, CONNECT)
 
 	fmt.Println("Exiting...")
 }
